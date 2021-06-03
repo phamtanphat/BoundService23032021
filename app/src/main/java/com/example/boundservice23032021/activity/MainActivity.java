12bc0442de11c639577880adde309094928245ab
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-03 21:24:10 +0700

    ui song

--- app/src/main/java/com/example/boundservice23032021/activity/MainActivity.java
+++ app/src/main/java/com/example/boundservice23032021/activity/MainActivity.java
@@ -1,16 +1,54 @@
 package com.example.boundservice23032021.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.boundservice23032021.R;
+import com.example.boundservice23032021.adapter.SongAdapter;
+import com.example.boundservice23032021.model.Song;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRcvSong;
+    List<Song> mListSong;
+    SongAdapter mSongAdapter;
+    String[] arrNameSong = {"chieuthuhoabongnang", "codocvuong", "hoahaiduong", "hoyeuaimatroi", "tetdongday"};
+    MediaMetadataRetriever mMetaRetriever;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRcvSong = findViewById(R.id.recyclerViewSong);
+        mListSong = new ArrayList<>();
+
+        mListSong.add(new Song("Chiều thu họa bóng nàng", R.raw.chieuthuhoabongnang, 0));
+        mListSong.add(new Song("Cô độc vương", R.raw.codocvuong, 0));
+        mListSong.add(new Song("Hoa hải đường", R.raw.hoahaiduong, 0));
+        mListSong.add(new Song("Họ yêu ai mất rồi", R.raw.hoyeuaimatroi, 0));
+        mListSong.add(new Song("Tết đong đầy", R.raw.tetdongday, 0));
+
+        for (int i = 0; i < arrNameSong.length; i++) {
+            Uri mediaPath = Uri.parse("android.resource://com.example.boundservice23032021/raw/" + arrNameSong[i]);
+            mMetaRetriever = new MediaMetadataRetriever();
+            mMetaRetriever.setDataSource(this, mediaPath);
+            long duration = Long.parseLong(mMetaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            mListSong.get(i).setDuration(duration);
+        }
+
+        mSongAdapter = new SongAdapter(mListSong);
+
+
+        mRcvSong.setAdapter(mSongAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/boundservice23032021/adapter/SongAdapter.java
@@ -0,0 +1,58 @@
+package com.example.boundservice23032021.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.boundservice23032021.R;
+import com.example.boundservice23032021.model.Song;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder>{
+
+    List<Song> mListSong;
+
+    public SongAdapter(List<Song> mListSong) {
+        this.mListSong = mListSong;
+    }
+
+    @NonNull
+    @Override
+    public SongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View v = layoutInflater.inflate(R.layout.layout_item_song,parent,false);
+        return new SongViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
+        Song song = mListSong.get(position);
+        holder.txtIndex.setText(position + 1 + "");
+        holder.txtTiTle.setText(song.getTitle());
+        long minus = (song.getDuration() / 60000) ;
+        long second = (song.getDuration() % 60000) / 1000;
+        holder.txtDuration.setText( "0" +minus + " : " + (second >= 10 ? second : "0" +second));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListSong != null ? mListSong.size() : 0;
+    }
+
+    class SongViewHolder extends RecyclerView.ViewHolder{
+        TextView txtIndex,txtTiTle,txtDuration;
+        public SongViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtIndex = itemView.findViewById(R.id.textViewIndex);
+            txtTiTle = itemView.findViewById(R.id.textViewTitle);
+            txtDuration = itemView.findViewById(R.id.textViewDuration);
+        }
+    }
+}
